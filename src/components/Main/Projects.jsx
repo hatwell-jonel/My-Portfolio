b11601cdba75af75bc1bacefb39913fcf7b43419
Jonel Hatwell <>
2022-06-13 22:04:02 +0800

    added show more button

--- src/components/Main/Projects.jsx
+++ src/components/Main/Projects.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { projects } from "../../data/projects";
 
 function Projects() {
+  const [noOfElements, setNoOfElelments] = useState(3);
+  const slice = projects.slice(0, noOfElements);
+
+  const showMore = () => {
+    setNoOfElelments(noOfElements + 3);
+  };
+
   return (
     <article className="projects">
       <div className="container">
         <p className="section_title">projects</p>
 
         <section className="projects_items">
-          {projects.map((item) => {
+          {slice.map((item) => {
             const {
               id,
               title,
@@ -36,6 +43,14 @@ function Projects() {
             );
           })}
         </section>
+
+        {noOfElements !== 9 ? (
+          <div className="more_button">
+            <button type="button" onClick={showMore}>
+              Show More...
+            </button>
+          </div>
+        ) : null}
       </div>
     </article>
   );
