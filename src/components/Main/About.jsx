91ec03db88f3e40d844a4fc2fc42813d7ffd99d3
Jonel Hatwell <>
2022-06-16 12:48:13 +0800

    added infomation about me

--- src/components/Main/About.jsx
+++ src/components/Main/About.jsx
@@ -13,13 +13,15 @@ function About() {
           </div>
           <div className="about_info-bottom">
             <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis
-              iure facere, necessitatibus et rem illum molestias, aspernatur
-              odit voluptate aperiam temporibus quasi cupiditate minus
-              dignissimos maiores quia. Deserunt, hic obcaecati nostrum nesciunt
-              exercitationem qui adipisci maiores rem aperiam animi id excepturi
-              earum ullam reprehenderit, molestias error consequatur tempora,
-              suscipit vel.
+              Hello ✋! I'm Jonel Hatwell, an enthusiastic front-end developer.
+              Currently 4th year college student under the program of Bachelor
+              of Science in Information Technology (BSIT) at City of Malabon
+              university, I am expecting to be graduated this year (2022).
+              <br />
+              <br />I have fundamental knowledge on front-end technologies like
+              strong ReactJs, Figma, and Boostrap etc,. What I like the most
+              about this field of development is I can express my creativity and
+              logical thinking skills, which gives me the feeling of enthusiasm.
             </p>
 
             <a className="download-btn" title="resumé" tabIndex={2}>
